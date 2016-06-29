9ac7dc9514b1bb5286b504103b4bf29beb5f2c85
Lucas <lucasakonopka@gmail.com>
2016-06-29 10:46:44 -0500

    Refactor revealPoint-assignment22 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,15 @@
 var animatePoints = function () {
     var points = document.getElementsByClassName('point');
 
+    var revealPoint = function (point) {
+        points[i].style.opacity = 1;
+        points[i].style.transform = "scaleX(1) translateY(0)";
+        points[i].style.msTransform = "scaleX(1) translateY(0)";
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    }
+    
     for (i = 0; i < points.length; i++) {
-        console.log(points[i]);
-        var revealPoints = function () {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scaleX(1) translateY(0)";
-            points[i].style.msTransform = "scaleX(1) translateY(0)";
-            points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-        }
-        revealPoints();
+        revealPoint(points[i]);
     }
 
 
