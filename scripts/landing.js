04a63715c30dc188c622a1cf2071b7c941e9ab34
Lucas <lucasakonopka@gmail.com>
2016-06-29 12:04:44 -0500

    assignment23 complete- implement for each method

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,22 @@
 var pointsArray = document.getElementsByClassName('point');
 
+
+var revealPoint = function (point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+
 var animatePoints = function (points) {    
-    var revealPoint = function (point) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
     
+    /*
     for (i = 0; i < points.length; i++) {
         revealPoint(points[i]);
     }
+    */
+    
+    forEach(points, revealPoint);
 
 
 }
